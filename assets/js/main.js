09064695688f3a45a18d5881670df40f5639a0ac
SAMO <samorj1783@gmail.com>
2021-04-06 19:06:08 -0500

    Plataforma Estadistica Disafilpa Version 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+/** code by webdevtrick.com ( https://webdevtrick.com ) **/
+var validateEmail = function(elementValue) {
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(elementValue);
+}
+$('#email').keyup(function() {
+
+    var value = $(this).val();
+    var valid = validateEmail(value);
+
+    if (!valid) {
+        $(this).css('color', 'red');
+		$('.addbut1').prop('disabled', true);
+    } else {
+        $(this).css('color', '#0074d2');
+		$('.addbut1').prop('disabled', false);
+    }
+});
+
+const newsletter = {};
+
+newsletter.main = document.querySelector('.subsc');
+newsletter.form = document.querySelector('.subsc > #singular-form');
+newsletter.subs = document.querySelector('.subsc > #singular-form > button#subs');
+newsletter.input = document.querySelector('.subsc>#singular-form>#email-input>input');
+newsletter.submitButton = document.querySelector('.subsc > #singular-form > #email-input > button');
+newsletter.successMessage = document.querySelector('.subsc > #singular-form > #success');
+
+newsletter.submitDelay = 1000;
+
+newsletter.clickHandler = (e) => {
+    switch (e.target) {
+        case newsletter.subs:
+            console.log('case subs');
+            newsletter.main.style.width = '28rem'
+            e.target.classList.remove('shown');
+            newsletter.input.classList.add('shown');
+            newsletter.submitButton.classList.add('shown');
+            newsletter.input.focus();
+            break;
+        case newsletter.submitButton:
+            newsletter.submitForm();
+            break;
+    }
+}
+newsletter.handleInputKeypress = (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        newsletter.submitForm();
+    }
+}
+newsletter.submitForm = () => {
+    newsletter.input.style.transition = 'all .6s ease';
+    newsletter.submitButton.style.transition = 'all .6s ease';
+    newsletter.input.classList.remove('shown');
+    newsletter.submitButton.classList.remove('shown');
+    newsletter.main.style.transition = 'all .6s cubic-bezier(0.47, 0.47, 0.27, 1.20) .4s';
+    newsletter.main.style.width = '';
+    newsletter.successMessage.classList.add('shown');
+    let submission = setTimeout(() => newsletter.form.submit(), newsletter.submitDelay);
+}
+
+newsletter.input.addEventListener('keypress', (e) => newsletter.handleInputKeypress(e));
+document.addEventListener('click', (e) => newsletter.clickHandler(e));
---
+++ assets/js/plugin/jquery.app.js
@@ -0,0 +1,133 @@
+
+!function($) {
+    'use strict';
+
+    var App = function() {
+        this.$body = $('body'),
+        this.$window = $(window)
+    };
+
+
+    /** 
+     * Initlizes the menu - top and sidebar
+    */
+    App.prototype.initMenu = function() {
+        var $this = this;
+
+        // Left menu collapse
+        $('.button-menu-mobile').on('click', function (event) {
+            event.preventDefault();
+            $this.$body.toggleClass("enlarged");
+           
+            // sidebar - scroll container
+            $('.slimscroll-menu').slimscroll({
+                height: 'auto',
+                position: 'right',
+                size: "8px",
+                color: '#9ea5ab',
+                wheelStep: 5,
+                touchScrollStep: 50
+            });
+        });
+
+        // Topbar - main menu
+        $('.navbar-toggle').on('click', function (event) {
+            $(this).toggleClass('open');
+        });
+
+        //metis menu
+        $("#side-menu").metisMenu();
+
+        // sidebar - scroll container
+        $('.slimscroll-menu').slimscroll({
+            height: 'auto',
+            position: 'right',
+            size: "8px",
+            color: '#9ea5ab',
+            wheelStep: 5,
+            touchScrollStep: 50
+        });
+
+        // right side-bar toggle
+        $('.right-bar-toggle').on('click', function(e){
+            $('body').toggleClass('right-bar-enabled');
+        });
+
+        $(document).on('click', 'body', function (e) {
+            if($(e.target).closest('.right-bar-toggle, .right-bar').length > 0) {
+                return;
+            }
+            $('body').removeClass('right-bar-enabled');
+            return;
+        });
+
+        // activate the menu in left side bar based on url
+        $("#sidebar-menu a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).addClass("active");
+                $(this).parent().addClass("active"); // add active to li of the current link
+                $(this).parent().parent().addClass("in");
+                $(this).parent().parent().prev().addClass("active"); // add active class to an anchor
+                $(this).parent().parent().parent().addClass("active");
+                $(this).parent().parent().parent().parent().addClass("in"); // add active to li of the current link
+                $(this).parent().parent().parent().parent().parent().addClass("active");
+            }
+        });
+
+        //Capture clic and Breadcrumb for SERFOR
+        $('a').on( "click", function(event) {
+            if(this.dataset.urlframe){   
+                $('a[data-urlframe]').parent().removeClass('active');   
+                $("#marco").attr('src', this.dataset.urlframe);
+                $(this).parent().addClass("active");
+                $('.page-title').html($(this).html());
+                var aux1=$(this).closest('.nav').closest('.collapse').closest('.nav-item').prevAll('.nav-section').children('.text-section'),
+					aux=$(this).closest('.nav').closest('.collapse').closest('.nav-item').find('.text-subtitle');
+                $('#breadcrumb-title').html(aux1.html());
+                $('#breadcrumb-subtitle').html(aux.html());
+
+                 }
+                }); 
+    },
+
+    /** 
+     * Init the layout - with broad sidebar or compact side bar
+    */
+    App.prototype.initLayout = function() {
+        var $this = this;
+        // in case of small size, add class enlarge to have minimal menu
+        if ($this.$window.width() < 1025) {
+            $this.$body.addClass('enlarged');
+        } else {
+            if ($this.$body.data('keep-enlarged') != true)
+                $this.$body.removeClass('enlarged');
+        }
+    },
+
+    //initilizing
+    App.prototype.init = function() {
+        var $this = this;
+        this.initLayout();
+        this.initMenu();
+
+        // handle responsiveness when reload
+        this.$window.on('resize', function(e) {
+            e.preventDefault();
+            $this.initLayout();
+        });
+    },
+
+    $.App = new App, $.App.Constructor = App
+
+
+}(window.jQuery),
+    //initializing main application module
+function($) {
+
+    $(function(){
+        //"use strict";
+        $.App.init();
+    });
+    
+}(window.jQuery);
